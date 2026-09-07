24cb3923068ab930e9a3919a6a3ddda0814aeb8b
zoecorral <zoe.corral@mi.unc.edu.ar>
2026-09-07 20:41:12 -0300

    Add constructor, destructor, and shipment methods
    Implement constructor and destructor for CentroDeDistribucion class. Add methods to show pending shipments and register new shipments.

--- GrupoD/CentroDeDistribucion.cpp
+++ GrupoD/CentroDeDistribucion.cpp
@@ -1,1 +1,35 @@
+#include "CentroDeDistribucion.hpp"
+
+CentroDeDistribucion:: CentroDeDistribucion() {
+
+
+CentroDeDistribucion:: ~CentroDeDistribucion() {
+  for (int i = 0; i < capacidad; i++) {
+            delete registro[i];
+        }
+
+        delete[] registro;
+}
+
+void CentroDeDistribucion:: mostrarPendientes() {
+  pendientes.mostrar();
+}
+
+void CentroDeDistribucion:: registrarNuevoEnvio(std::string codigo, std::string destinatario, std::string zona, float peso, nivelDeServicio nivel() {
+   if (cantidadRegistrados == capacidad) {
+            redimensionar();
+        }
+
+        Envio* nuevo = new Envio*(codigo, destinatario, zona, peso, nivel);
+
+        registro[cantidadRegistrados] = nuevo;
+        cantidadRegistrados++;
+
+        pendientes.agregarPorPrioridad(nuevo);
+
+        std::cout << "Envío " << codigo << " registrado correctamente." << std::endl;
+}
+
+}
+
 

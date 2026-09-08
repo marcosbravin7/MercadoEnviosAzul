8ace22c2eced46030a1db76ad4da1e84792427e9
zoecorral <zoe.corral@mi.unc.edu.ar>
2026-09-07 21:06:42 -0300

    Update CentroDeDistribucion.hpp

--- GrupoD/CentroDeDistribucion.hpp
+++ GrupoD/CentroDeDistribucion.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-
+#inclue "ListaPendientes.hpp"
 
 class CentroDeDistribucion {
 private:
@@ -11,12 +11,17 @@ private:
     int cantidadRegistrados;
 
     void redimensionar () {
-        int nuevaCapacidad = capacidad * 2;
+        int nuevaCapacidad = capacidad * 2; //duplicamos la memoria
 
-        Envio** nuevoRegistro =  new Envio*[capacidad];
-        for (int i = 0; i < capacidad; i++) {
+        Envio** nuevoRegistro =  new Envio*[capacidad]; //reserva la cantidad de la nueva memoria
+        
+        for (int i = 0; i < cantidadRegistrados; i++) {
             nuevoRegistro[i] = registro[i];
-        }
+        } //copiamos punteros existentes
+
+        for (int i = cantidadRegostrados; i  < nuevaCapacidad; i++) {
+            nuevoRegistro[i] = nullptr;
+        } //el resto del espacio en nullptr
 
         delete[] registro;
         registro = nuevoRegistro;
@@ -24,15 +29,20 @@ private:
     }
 
 public:
-    CentroDeDistribucion (int capacidadInicial) : capacidad (capacidadInicial), cantidadRegistrados (0) {}
+    CentroDeDistribucion (int capacidadInicial = 10) : capacidad (capacidadInicial), cantidadRegistrados (0) {
+        registro = new Envio*[capacidad];
+        for (int i = 0; i < capacidad; i++) {
+            registro[i] = nullptr;
+        }
+    }
 
-    ~CentroDeDistribucion ();
+    ~CentroDeDistribucion () ;
 
     void mostrarPendientes ();
 
     void registrarNuevoEnvio (std::string codigo, std::string destinatario, std::string zona, float peso, nivelDeServicio nivel);
 
-    //buscarEnvio
+    Envio* buscarEnvio (std::string codigo);
 
     void cambiarEstado (std::string codigo, Estados nuevoEstado);
 

2f3af8c71a6f6692582f867358650e0152827330
zoecorral <zoe.corral@mi.unc.edu.ar>
2026-09-08 00:57:34 -0300

    Initialize CentroDeDistribucion and add methods

--- GrupoD/CentroDeDistribucion.cpp
+++ GrupoD/CentroDeDistribucion.cpp
@@ -1,7 +1,13 @@
 #include "CentroDeDistribucion.hpp"
 
 CentroDeDistribucion:: CentroDeDistribucion() {
-
+  capacidad = 10;
+  cantidadRegistrados = 0;
+  registro = new Envio*[capacidad];
+  for (int i = 0; i < capacidad; i++) {
+      registro[i] = nullptr;
+  }
+}
 
 CentroDeDistribucion:: ~CentroDeDistribucion() {
   for (int i = 0; i < capacidad; i++) {
@@ -30,6 +36,47 @@ void CentroDeDistribucion:: registrarNuevoEnvio(std::string codigo, std::string 
         std::cout << "Envío " << codigo << " registrado correctamente." << std::endl;
 }
 
+Envio* CentroDeDistribucion :: buscarEnvio (std::string codigo) {
+    return listaEnvios.buscar(codigo);
+}
+
+void CentroDeDistribucion:: cambiarEstado (std::string codigo, Estados nuevoEstado, std::string observacion) {
+  Envio* paquete = nullptr;
+    for (int i = 0; i < cantidadRegistrados; i++) {
+        if (registro[i] != nullptr && registro[i]->getCodigo() == codigo) {
+            paquete = registro[i];
+            break;
+        }
+    }
+
+  if (paquete == nullptr) {
+    std::cout << "No se encontro ningun envio"  << std::endl;
+    return;
+  }
+
+  paquete->cambiarEstado(nuevoEstado, observacion);
+  std:: cout << "Estado cambiado correctamente de envio: " << codigo << std::endl;
+}
+
+void CentroDeDistribucion:: despacharProximoEnvio() {
+
+}
+
+void CentroDeDistribucion :: reprogramarEnvio(std::string codigo, std:: string nuevaZona) {
+  
+}
+
+void CentroDeDistribucion :: mostrarHistorial (std::string codigo) {
+  Envio* paquete = buscarEnvio(codigo);
+
+  if (paquete == nullptr) {
+    std:: cout << "No se encontro el envio" << std::endl;
+    return;
+  }
+
+  paquete.mostrarHistorialCronologico();
+}
+
 void CentroDeDistribucion:: resumenRecursivoPorZona (std::string zona) {
   ResumenZona reporte = pendientes.resumenPorZona(zona);
 
@@ -41,6 +88,15 @@ void CentroDeDistribucion:: resumenRecursivoPorZona (std::string zona) {
 
 }
 
+void CentroDeDistribucion :: finalizarEntrega(std::string codigo) {
+  Envio* paquete = buscarEnvio(codigo);
+
+  if (paquete == nullptr) {
+    std:: cout << "No se encontró el envío" << std::endl;
+  }
+
+  paquete->cambiarEstado(Estado::ENTREGADO , "Entrega finalizada");
+  std::cout << "Envio " << codigo << " marcado como ENTREGADO" << std::endl;
 }
 
-
+}
